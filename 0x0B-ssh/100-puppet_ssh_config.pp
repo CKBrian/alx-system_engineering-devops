d3323b7543aaa06ee84ee2d7e4973830dd905899
Brian <chumobrian@gmail.com>
2024-01-01 21:52:52 +0300

    feat: Add a manifest file that configures the SSH client to use the private key ~/.ssh/school and refuse to authenticate using a password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,23 @@
 #  command => '/usr/bin/mkdir -p ~/.ssh',
 #}
 # configures SSH client to use the private key ~/.ssh/school and refuse to authenticate using a password
-file {'~/.ssh/ssh_config':
-  ensure  => 'present',
-  mode    => '0600',
-  content => @("SSH_CONFIG")
-    Host dev
-      HostName 54.197.44.18
-      User ubuntu
-      IdentityFile '~/.ssh/school'
-      PasswordAuthentication no
-    SSH_CONFIG
+#file {'~/.ssh/ssh_config':
+#  ensure  => 'present',
+#  mode    => '0600',
+#  content => @("SSH_CONFIG")
+#    Host dev
+#      HostName 54.197.44.18
+#      User ubuntu
+#      IdentityFile '~/.ssh/school'
+#      PasswordAuthentication no
+#    SSH_CONFIG
+#}
+file_line {'turn off passwd auth':
+  path => '~/.ssh/ssh_config'
+  line => 'PasswordAuthentication no'
+}
+
+file_line {'Identity file':
+  path => '~/.ssh/ssh_config'
+  line => IdentityFile '~/.ssh/school'
 }
