73fcef36b3d31a818392111485820d1ddd523d97
Brian <chumobrian@gmail.com>
2024-01-04 19:25:26 +0300

    Add a manifest script that configures a server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -21,25 +21,23 @@ file {'/var/www/html/index.html':
   ensure  => 'file',
   content => "Hello World!",
 }
-# echo "Hello World!" | sudo tee /var/www/html/index* > /dev/null
 
 # configures redirect_me
 exec {'permanent_redirect':
   command => "sudo sed -i '/listen 80 default_server;/ a\\n\t rewrite ^/ /var/www/html/index.html permanent;\n' /etc/nginx/sites-available/default,"
   notify  => Service['nginx'],
-
 }
 # create a custon 404 not found page
-file {'/var/www/html/custom_404.html':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-}
+#file {'/var/www/html/custom_404.html':
+#  ensure  => 'file',
+#  content => "Ceci n'est pas une page",
+#}
 # echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html > /dev/null
 
-exec {'404_not_found':
-  command => 'sudo sed -i "s/server {/server {\n    error_page 404 \/custom_404.html;/g" /etc/nginx/sites-available/default',
-  notify  => Service['nginx'],
-}
+#exec {'404_not_found':
+#  command => 'sudo sed -i "s/server {/server {\n    error_page 404 \/custom_404.html;/g" /etc/nginx/sites-available/default',
+#  notify  => Service['nginx'],
+#}
 
 # reload Nginx configs
 # sudo service nginx restart
