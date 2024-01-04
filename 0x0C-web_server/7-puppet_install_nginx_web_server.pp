3e1a37ecc13f44669a4e848190390370cb97c08e
Brian <chumobrian@gmail.com>
2024-01-04 20:16:18 +0300

    Add a manifest script that configures a server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,78 +1,61 @@
 # NGINX server
-exec {'update':
-command => 'sudo apt-get update -y',
+
+# Update packages
+exec { 'update':
+  command => 'apt update -y',
+  path    => '/usr/bin/apt',
 }
 
-# installs nginx and ensures its running
-package {'nginx':
+# Install NGINX and ensure it's running
+package { 'nginx':
   ensure => 'installed',
 }
-service {'nginx':
+
+service { 'nginx':
   ensure => 'running',
   enable => true,
 }
-# configure Nginx to be listening on port 80
-exec {'Nginx_port_config':
-  command => 'sudo ufw allow "Nginx HTTP"',
+
+# Configure Nginx to listen on port 80
+exec { 'Nginx_port_config':
+  command => 'ufw allow "Nginx HTTP"',
+  path    => '/usr/bin/ufw',
 }
 
-# configure Nginx server to return a page that contains the string Hello World!
-file {'/var/www/html/index.html':
+# Configure NGINX server
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+
+      rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+      error_page 404 /custom_404.html;
+
+      root /var/www/html;
+
+      index index.html index.htm index.nginx-debian.html;
+
+      server_name _;
+
+      location / {
+        try_files \$uri \$uri/ =404;
+      }
+    }
+  ",
+  notify => Service['nginx'],
+}
+
+# Create a Hello World page
+file { '/var/www/html/index.html':
   ensure  => 'file',
   content => 'Hello World!',
 }
 
-# create a custon 404 not found page
-file {'/var/www/html/custom_404.html':
+# Create a custom 404 page
+file { '/var/www/html/custom_404.html':
   ensure  => 'file',
   content => "Ceci n'est pas une page",
 }
-# configures redirect_me
-file {'/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "
-	server {
-			listen 80 default_server;
-			listen [::]:80 default_server;
 
-			rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-			error_page 404 /custom_404.html;
-
-			root /var/www/html;
-
-			# Add index.php to the list if you are using PHP
-			index index.html index.htm index.nginx-debian.html;
-
-			server_name _;
-
-			location / {
-					# First attempt to serve request as file, then
-					# as directory, then fall back to displaying a 404.
-					try_files \$uri \$uri/ =404;
-			}
-
-	}
-	  ",
-  notify  => Service['nginx'],
-
-}
-#exec {'permanent_redirect':
-#  command => "
-#    sudo sed -i '/listen 80 default_server;/ a\\n\t rewrite ^/ /var/www/html/index.html permanent;\n' /etc/nginx/sites-available/default
-#	",
-#  notify  => Service['nginx'],
-#}
-# create a custon 404 not found page
-#file {'/var/www/html/custom_404.html':
-#  ensure  => 'file',
-#  content => "Ceci n'est pas une page",
-#}
-# echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html > /dev/null
-
-#exec {'404_not_found':
-#  command => 'sudo sed -i "s/server {/server {\n    error_page 404 \/custom_404.html;/g" /etc/nginx/sites-available/default',
-#  notify  => Service['nginx'],
-#}
-
-# reload Nginx configs
-# sudo service nginx restart
