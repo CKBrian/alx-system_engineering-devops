2b8035f7b809423f400a56003872bb588ccd0057
Brian <chumobrian@gmail.com>
2024-01-09 01:48:01 +0300

    Add a manifest script that configures a server header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,7 +17,7 @@ service { 'nginx':
 file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
   match => 'http {;',
-  line  => "http {\n\tadd_header X-Served-By hostname;",
+  line  => "http {\n\tadd_header X-Served-By $HOSTNAME;",
 }
 
 # Reload NGINX to apply the changes
