0d638e733af1a914fbd55b30df7f1980be2989d9
Brian <chumobrian@gmail.com>
2024-01-09 22:32:21 +0300

    Add a manifest script that configures a server header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,19 @@
-# Update packages
+#puppet advance
 exec { 'update':
-  command => 'apt update -y',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-# Install NGINX and ensure it's running
-package { 'nginx':
-  ensure => 'installed',
-}
-
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-}
-
-# Add Custom HTTP Response Header and Reload NGINX to apply the changes
-exec { 'nginx_reload':
-  command  => '/bin/bash -c "apt-get -y install nginx; \
-                sed -i \'/server_name _;/a\\\n\tadd_header X-Served-By $HOSTNAME;/\' \
-                /etc/nginx/sites-available/default"',
+  command  => 'sudo apt-get update',
   provider => shell,
 }
-
-exec { 'restart service':
+-> package {'nginx':
+  ensure => present,
+}
+-> file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
+}
+-> exec { 'restart service':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
-
