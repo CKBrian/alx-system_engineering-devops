adee6c499b1267b584eecb1de832b66e4b9c17d8
Brian <chumobrian@gmail.com>
2024-01-29 16:40:43 +0300

    feat: Add a  module that uses REST API, for a given employee ID & returns information about his/her TODO list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,16 +12,13 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users?id={}".format(E_id)
     res = requests.get(url)
     name = res.json()[0].get('name')
-
     # extracts employee data
     url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(E_id)
     with urlopen(url) as resp:
         data = resp.read().decode("utf-8")
     emp_data = json.loads(data)
-
     # list of completed titles
     titles = [item.get('title') for item in emp_data if item.get('completed')]
-
     tasks = len(emp_data)
     c_tasks = len(titles)
     print("Employee {} is done with tasks({}/{})".format(name, c_tasks, tasks))
