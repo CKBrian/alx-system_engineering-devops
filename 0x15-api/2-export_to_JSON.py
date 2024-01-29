03de1bb48c2a62523e0e3677671d90dedc1dc82a
Brian <chumobrian@gmail.com>
2024-01-29 22:17:33 +0300

    feat: Add a  module that uses REST API, for a given employee ID & returns information about his/her TODO list progress and Records all tasks that are owned by this employee in a json file

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' Records all tasks that are owned by an employee '''
+import json
+import requests
+import sys
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    E_id = sys.argv[1]
+    filename = "{}.json".format(E_id)
+
+    # extracts employee name
+    url = "https://jsonplaceholder.typicode.com/users?id={}".format(E_id)
+    res = requests.get(url)
+    name = res.json()[0].get('username')
+
+    # extracts employee data
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(E_id)
+    with urlopen(url) as resp:
+        data = resp.read().decode("utf-8")
+    emp_data = json.loads(data)
+
+    # Format{ "USER_ID": [{"task": "TASK_TITLE", "completed":
+    #                  TASK_COMPLETED_STATUS, "username": "USERNAME"},{... ]}
+    titles = [{"task": item.get('title'), "completed": item.get('completed'),
+               "username": name} for item in emp_data]
+    title_dict = dict(E_id: titles)
+    print(title_dict)
+    # write to json
+    with open(filename, mode='w', encoding="utf-8") as f:
+        json.dump(title_dict, filename)
