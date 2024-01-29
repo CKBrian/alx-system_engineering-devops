edba4eaba6f076c6b815d4564811eb4f8757bbd1
Brian <chumobrian@gmail.com>
2024-01-29 17:48:02 +0300

    feat: Add a  module that uses REST API, for a given employee ID & returns information about his/her TODO list progress and Records all tasks that are owned by this employee

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' Records all tasks that are owned by an employee '''
+import csv
+import json
+import requests
+import sys
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    E_id = sys.argv[1]
+    filename = "{}.csv".format(E_id)
+
+    # extracts employee name
+    url = "https://jsonplaceholder.typicode.com/users?id={}".format(E_id)
+    res = requests.get(url)
+    name = res.json()[0].get('username')
+
+    # extracts employee data
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(E_id)
+    with urlopen(url) as resp:
+        data = resp.read().decode("utf-8")
+    emp_data = json.loads(data)
+
+    # Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    titles = [{'userId': str(item.get('userId')), 'username': str(name),
+               'completed': str(item.get('completed')),
+               'title': str(item.get('title'))} for item in emp_data]
+
+    # write to csv
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f)
+        for item in titles:
+            writer.writerow(item.values())
