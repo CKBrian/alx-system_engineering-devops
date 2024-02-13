4a2b2e0b868ff30430d9d1f8948df5c534d07260
Brian <chumobrian@gmail.com>
2024-02-13 20:43:03 +0300

    feat: Add a function that returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     resp = requests.get(url, headers={'User-Agent': 'CKBrian7'})
-    if 'error' in resp.keys():
+    if 'error' in resp.json().keys():
         return 0
     if resp.status_code == 200:
         return resp.json().get('data').get('subscribers')
