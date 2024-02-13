5efaa54756cd1a8e84d1d9257fe593da4044c105
Brian <chumobrian@gmail.com>
2024-02-13 20:07:43 +0300

    feat: Add a function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -11,11 +11,11 @@ def word_tally(word, hot_list):
     return count
 
 
-def recurse(subreddit, word_list, hot_list=[], next_page=None):
+def recurse(subreddit, word_list, hot_list=[], new_page=""):
     """returns titles of the first 10 hot posts
        listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
-                                                                 next_page)
+                                                                 new_page)
     resp = requests.get(url, headers={'User-Agent': 'Holberton_8407'},
                         allow_redirects=False)
     if resp.status_code == 200:
@@ -23,14 +23,17 @@ def recurse(subreddit, word_list, hot_list=[], next_page=None):
         new_list = [title.get('data').get('title') for title in data]
         hot_list.extend(new_list)
 
-        new_page = resp.json().get('data').get('after')
-        recurse(subreddit, word_list, hot_list, new_page)
-
         # count the words in titles
         word_dict = {word: word_tally(word, hot_list) for word in word_list}
-        return '\n'.join(f"{key}: {value}" for key, value in word_dict.items())
+        final_str = '\n'.join(f"{key}: {value}"
+                              for key, value in word_dict.items())
+
+        new_page = resp.json().get('data').get('after')
+        if not new_page:
+            return final_str
     else:
         return None
+    return recurse(subreddit, word_list, hot_list, new_page)
 
 
 def count_words(subreddit, word_list):
