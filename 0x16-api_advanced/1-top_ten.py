52503164ff25f5b1acbd6b158c333eef2ec6d809
Brian <chumobrian@gmail.com>
2024-02-13 13:56:57 +0300

    feat: Add a function that returns titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""""
+import requests
+
+
+def top_ten(subreddit):
+    """returns titles of the first 10 hot posts
+       listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    resp = requests.get(url, headers={'User-Agent': 'Leather_Bed_8407'})
+    if resp.status_code == 200:
+        data = resp.json().get('data').get('children')
+        top_posts = [title.get('data').get('title') for title in data]
+        posts = top_posts[:10]
+        return '\n'.join(post for post in posts)
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    print(top_ten('programming'))
