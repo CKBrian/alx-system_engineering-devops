6bdc97052e10afb1e0507110f225fe5bd0674d69
Brian <chumobrian@gmail.com>
2024-02-13 12:33:28 +0300

    feat: Add a function that returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers in the Reddit API"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    resp = requests.get(url, headers={'User-Agent':'Leather_Bed_8407'})
+    resp = requests.get(url, headers={'User-Agent': 'Leather_Bed_8407'})
     if resp == 'OK':
         return resp.json().get('data').get('subscribers')
     else:
