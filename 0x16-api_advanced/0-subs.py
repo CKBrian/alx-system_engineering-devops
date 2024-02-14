434766ef465d8d75737d4886bf76352a11fc730a
Brian <chumobrian@gmail.com>
2024-02-14 10:23:41 +0300

    feat: Add a function that returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    res = requests.get(url, headers={'User-Agent': 'CKbrian'}).json()
+    res = requests.get(url, headers={'User-Agent': 'DreMukare'}).json()
     count = 0
     if 'error' not in res.keys():
         count = res.get('data').get('subscribers')
