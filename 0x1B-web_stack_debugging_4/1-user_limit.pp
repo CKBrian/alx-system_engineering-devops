760babfe7bfcda3c83b68778b246050f7e11952d
Brian <chumobrian@gmail.com>
2024-02-24 10:56:13 +0300

    feat: Add a puppet manifest file that configures the OS so that it is possible to login with the holberton user and open a file without any error message.

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# file limit(hard) for Holberton user.
+exec { 'holberton-user-file-limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# file limit(soft) for Holberton user.
+exec { 'holberton-user-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
