21d80676eb3bff495220417c7117a65d64604e47
Brian <chumobrian@gmail.com>
2024-02-24 11:12:24 +0300

    feat: Add a puppet manifest file that configures nginx to exter the open files ULIMIT to 4096

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,4 @@
+# Increases U-LIMIT in nginx configuration to allow handling of 1000 requests
 exec { 'U-limit':
   command => 'sudo sed -i s/15/4096/ /etc/default/nginx && sudo service nginx restart',
   path    => ['/usr/bin', '/usr/sbin'],
